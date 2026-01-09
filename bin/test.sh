aedb6bde5985b99099575050c8544107bc8e4bc7
Marlon Baeten <marlon@tweedegolf.com>
2026-01-09 12:20:34 +0100

    Fix docker flags

--- bin/test.sh
+++ bin/test.sh
@@ -22,5 +22,5 @@ cargo build \
     --bin eks
 
 pushd playwright
-    docker compose run --quiet-pull --quiet-build playwright
+    docker compose run --rm --quiet-pull --quiet-build playwright
 popd
