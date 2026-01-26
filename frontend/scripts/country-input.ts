75b66a4ef9f713c02f037b321184544ea6d078be
Michiel <michielp1807@gmail.com>
2026-01-26 15:49:43 +0100

    Add basic unit test for country flags

--- frontend/scripts/country-input.ts
+++ frontend/scripts/country-input.ts
@@ -9,7 +9,7 @@ function getCountryOptions(): NodeListOf<HTMLOptionElement> {
   return document.querySelectorAll(COUNTRY_OPTION_SELECTOR);
 }
 
-function countryCodeToFlagEmoji(countryCode: string): string {
+export const countryCodeToFlagEmoji = (countryCode: string) => {
   if (/^[A-Z]{2}$/.test(countryCode)) {
     const codePoints = [...countryCode].map(
       (char) => 0x1f1e6 + char.charCodeAt(0) - 65,
@@ -19,32 +19,34 @@ function countryCodeToFlagEmoji(countryCode: string): string {
   } else {
     return "🌐";
   }
+};
+
+if (typeof window !== "undefined") {
+  window.addEventListener("load", () => {
+    // Make flag icon match country code input
+    const countryInputs = getCountryInputs();
+    countryInputs.forEach((input) => {
+      const textInput = input.querySelector("input");
+      const flagIcon = input.querySelector(".icon");
+
+      if (!textInput || !flagIcon) {
+        console.error("Country input missing text or icon element");
+        return;
+      }
+
+      const setFlagIcon = () => {
+        textInput.value = textInput.value.toUpperCase();
+        flagIcon.textContent = countryCodeToFlagEmoji(textInput.value);
+      };
+
+      textInput.addEventListener("input", setFlagIcon);
+      setFlagIcon();
+    });
+
+    // Add country flags to options
+    const options = getCountryOptions();
+    options.forEach((option) => {
+      option.innerText = `${countryCodeToFlagEmoji(option.value)} ${option.value}`;
+    });
+  });
 }
-
-window.addEventListener("load", () => {
-  // Make flag icon match country code input
-  const countryInputs = getCountryInputs();
-  countryInputs.forEach((input) => {
-    const textInput = input.querySelector("input");
-    const flagIcon = input.querySelector(".icon");
-
-    if (!textInput || !flagIcon) {
-      console.error("Country input missing text or icon element");
-      return;
-    }
-
-    const setFlagIcon = () => {
-      textInput.value = textInput.value.toUpperCase();
-      flagIcon.textContent = countryCodeToFlagEmoji(textInput.value);
-    };
-
-    textInput.addEventListener("input", setFlagIcon);
-    setFlagIcon();
-  });
-
-  // Add country flags to options
-  const options = getCountryOptions();
-  options.forEach((option) => {
-    option.innerText = `${countryCodeToFlagEmoji(option.value)} ${option.value}`;
-  });
-});
