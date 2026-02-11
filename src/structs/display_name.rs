48372a40625f8e6e554f5fe44a79e0fdc9beb995
Dré van Oorschot <dre.van.oorschot@technolution.nl>
2026-02-11 13:02:13 +0100

    [e-KS#238] remove trailing white space requirement from display name

--- src/structs/display_name.rs
+++ src/structs/display_name.rs
@@ -28,11 +28,7 @@ impl FromStr for DisplayName {
     type Err = ValidationError;
 
     fn from_str(value: &str) -> Result<Self, Self::Err> {
-        // TODO figure out why string misses trailing white spaces before entering this method
-        let has_trailing_space = value.ends_with(" ");
-        let mut trimmed_value = value.split_whitespace().collect::<Vec<&str>>().join(" ");
-        trimmed_value += if has_trailing_space { " " } else { "" };
-        dbg!(&trimmed_value, has_trailing_space);
+        let trimmed_value = value.split_whitespace().collect::<Vec<&str>>().join(" ");
         let char_count = trimmed_value
             .split_whitespace()
             .collect::<Vec<_>>()
@@ -73,16 +69,15 @@ mod tests {
 
     #[test]
     fn valid_name_with_extra_spaces() {
-        test_display_name("\t  De  \t  Tegen   Partij ", Ok("De Tegen Partij "));
+        test_display_name("\t  De  \t  Tegen   Partij ", Ok("De Tegen Partij"));
 
-        // TODO do we consider this a defect or is this expected outcome (tab is not a space)
         test_display_name("\t  De  \t  Tegen   Partij \t", Ok("De Tegen Partij"));
     }
 
     #[test]
     fn too_long() {
         test_display_name(
-            "a string of exactly 36 chars long exl spaces",
+            "a string of exactly 36 chars long ex. spaces",
             Err(ValidationError::ValueTooLong(36, 35)),
         );
     }
@@ -90,7 +85,7 @@ mod tests {
     #[test]
     fn too_short() {
         test_display_name(
-            "     f         ",
+            "     f   \t      ",
             Err(ValidationError::ValueTooShort(1, 35)),
         );
     }
