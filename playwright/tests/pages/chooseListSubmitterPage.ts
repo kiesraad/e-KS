1abd28018dc2af3dd3ba1a1ee4dea7fc45f49a60
grietje ogink <grietje.ogink@kiesraad.nl>
2026-04-02 15:03:09 +0200

    added test choose list submitter

---
+++ playwright/tests/pages/chooseListSubmitterPage.ts
@@ -0,0 +1,40 @@
+import type { Locator, Page } from "@playwright/test";
+import type { ListSubmitter } from "../models/listSubmitter";
+
+export class ChooseListSubmitterPage {
+  readonly linkElectoralDistrictsPage: Locator;
+  readonly buttonClose: Locator;
+  readonly buttonSave: Locator;
+
+  constructor(protected readonly page: Page) {
+    this.linkElectoralDistrictsPage = this.page.getByRole("link", {
+      name: "Kieskringen",
+    });
+    this.buttonClose = this.page.getByRole("link", { name: "Sluiten" });
+    this.buttonSave = this.page.getByRole("button", { name: "Opslaan" });
+  }
+
+  getSubmitterLocator(listSubmitter: ListSubmitter) {
+    return this.page.getByRole("radio", {
+      name: new RegExp(listSubmitter.lastName),
+    });
+  }
+
+  getSubstituteSubmitterLocator(listSubmitter: ListSubmitter) {
+    return this.page.getByRole("checkbox", {
+      name: new RegExp(listSubmitter.lastName),
+    });
+  }
+
+  async selectListSubmitter(listsubmitter: ListSubmitter) {
+    await this.getSubmitterLocator(listsubmitter).check();
+  }
+
+  async selectSubstituteSubmitter(listsubmitter: ListSubmitter) {
+    await this.getSubstituteSubmitterLocator(listsubmitter).check();
+  }
+
+  async removeSubstituteSubmitter(listsubmitter: ListSubmitter) {
+    await this.getSubstituteSubmitterLocator(listsubmitter).uncheck();
+  }
+}
