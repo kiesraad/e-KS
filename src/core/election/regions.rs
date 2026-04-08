9016ef2997c05ed7c200e458d14d63fff64b07b5
Dré van Oorschot <dre.van.oorschot@technolution.nl>
2026-04-08 14:07:04 +0200

    [#474] add title to province and water councils

--- src/core/election/regions.rs
+++ src/core/election/regions.rs
@@ -1,5 +1,7 @@
 use serde::{Deserialize, Serialize};
 
+use crate::{ElectoralDistrict as District, core::AnyLocale};
+
 /// Regions for the elections of the provincial councils
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize)]
 pub enum Province {
@@ -15,6 +17,26 @@ pub enum Province {
     ZE,
     NB,
     LI,
+}
+
+impl Province {
+    pub fn title(&self, locale: AnyLocale) -> &'static str {
+        let district = match self {
+            Self::GR => District::GR,
+            Self::FR => District::FR,
+            Self::DR => District::DR,
+            Self::OV => District::OV,
+            Self::GE => District::GE,
+            Self::FL => District::FL,
+            Self::UT => District::UT,
+            Self::NH => District::NH,
+            Self::ZH => District::ZH,
+            Self::ZE => District::ZE,
+            Self::NB => District::NB,
+            Self::LI => District::LI,
+        };
+        district.title(locale)
+    }
 }
 
 /// Regions for the elections of the water authorities
@@ -41,4 +63,33 @@ pub enum WaterCouncil {
     AaEnMaas,
     Limburg,
     Zuiderzeeland,
+}
+
+impl WaterCouncil {
+    pub fn title(&self, locale: AnyLocale) -> &'static str {
+        let district = match self {
+            Self::Noorderzijlvest => District::WsNoorderzijlvest,
+            Self::Fryslan => District::WsFryslan,
+            Self::HunzeEnAas => District::WsHunzeEnAas,
+            Self::DrentsOverijsselseDelta => District::WsDrentsOverijsselseDelta,
+            Self::Vechtstromen => District::WsVechtstromen,
+            Self::ValleiEnVeluwe => District::WsValleiEnVeluwe,
+            Self::RijnEnIJssel => District::WsRijnEnIJssel,
+            Self::DeStichtseRijnlanden => District::WsDeStichtseRijnlanden,
+            Self::AmstelGooiEnVecht => District::WsAmstelGooiEnVecht,
+            Self::HollandsNoorderkwartier => District::WsHollandsNoorderkwartier,
+            Self::Rijnland => District::WsRijnland,
+            Self::Delfland => District::WsDelfland,
+            Self::SchielandEnDeKrimpenerwaard => District::WsSchielandEnDeKrimpenerwaard,
+            Self::Rivierenland => District::WsRivierenland,
+            Self::HollandseDelta => District::WsHollandseDelta,
+            Self::Scheldestromen => District::WsScheldestromen,
+            Self::BrabantseDelta => District::WsBrabantseDelta,
+            Self::DeDommel => District::WsDeDommel,
+            Self::AaEnMaas => District::WsAaEnMaas,
+            Self::Limburg => District::WsLimburg,
+            Self::Zuiderzeeland => District::WsZuiderzeeland,
+        };
+        district.title(locale)
+    }
 }
