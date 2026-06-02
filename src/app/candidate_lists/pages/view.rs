13f66e2a862b6c1735b875312753dcb25824f065
Michiel <michielp1807@gmail.com>
2026-06-02 14:03:17 +0200

    Show duplicates on candidate list page

--- src/app/candidate_lists/pages/view.rs
+++ src/app/candidate_lists/pages/view.rs
@@ -2,8 +2,8 @@ use askama::Template;
 use axum::response::IntoResponse;
 
 use crate::{
-    AppError, Context, HtmlTemplate,
-    candidate_lists::{CandidateList, FullCandidateList, pages::ViewCandidateListPath},
+    AppError, AppStore, Context, ElectoralDistrict, HtmlTemplate,
+    candidate_lists::{CandidateList, CandidateListSummary, FullCandidateList, pages::ViewCandidateListPath},
     common::Problematic,
     filters,
 };
@@ -12,15 +12,26 @@ use crate::{
 #[template(path = "candidate_lists/pages/view.html")]
 struct CandidateListViewTemplate {
     full_list: FullCandidateList,
+    duplicate_districts: Vec<ElectoralDistrict>,
 }
 
 pub async fn view_candidate_list(
     _: ViewCandidateListPath,
     context: Context,
     full_list: FullCandidateList,
+    store: AppStore,
 ) -> Result<impl IntoResponse, AppError> {
+    let duplicate_districts = CandidateListSummary::list(&store)
+        .into_iter()
+        .find(|s| s.list.id == full_list.list.id)
+        .map(|s| s.duplicate_districts)
+        .unwrap_or_default();
+
     Ok(HtmlTemplate(
-        CandidateListViewTemplate { full_list },
+        CandidateListViewTemplate {
+            full_list,
+            duplicate_districts,
+        },
         context,
     ))
 }
@@ -53,6 +64,7 @@ mod tests {
             ViewCandidateListPath { list_id },
             Context::new_test_without_db(),
             full_list,
+            store,
         )
         .await?
         .into_response();
