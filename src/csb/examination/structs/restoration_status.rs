ebc02523740902ca86458ec841c3906290ebed96
Dré van Oorschot <dre.van.oorschot@technolution.nl>
2026-08-07 17:55:40 +0200

    [#960] add unit tests for restoration status

--- src/csb/examination/structs/restoration_status.rs
+++ src/csb/examination/structs/restoration_status.rs
@@ -48,3 +48,245 @@ impl RestorationStatus {
         self.has_corrections
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use crate::{
+        CsbEvent, ElectoralDistrict,
+        structs::{
+            common::{DisplayName, Initials},
+            csb::{Correction, OmissionCategory, PersonCorrection, sample_omission},
+        },
+        test_utils::{sample_candidate_list, sample_person},
+    };
+
+    use super::*;
+
+    #[test]
+    fn for_political_group_no_changes() {
+        let store = CsbStore::new_for_test();
+
+        let status = RestorationStatus::for_political_group(&store);
+
+        assert!(!status.has_omissions());
+        assert!(!status.has_corrections());
+    }
+
+    #[tokio::test]
+    async fn for_political_group_omission_and_correction() -> Result<(), AppError> {
+        let store = CsbStore::new_for_test();
+
+        store
+            .update(CsbEvent::CreateOmission(sample_omission(
+                OmissionCategory::PoliticalGroup,
+            )))
+            .await?;
+        store
+            .update(CsbEvent::UpdateCorrection(Correction::DisplayName(
+                DisplayName::from_str("Correction Party").unwrap(),
+            )))
+            .await?;
+
+        let status = RestorationStatus::for_political_group(&store);
+
+        assert!(status.has_omissions());
+        assert!(status.has_corrections());
+
+        Ok(())
+    }
+
+    #[test]
+    fn for_declarations_of_support_no_changes() {
+        let store = CsbStore::new_for_test();
+
+        let status = RestorationStatus::for_declarations_of_support(&store);
+
+        assert!(!status.has_omissions());
+        assert!(!status.has_corrections());
+    }
+
+    #[tokio::test]
+    async fn for_declarations_of_support_omission() -> Result<(), AppError> {
+        let store = CsbStore::new_for_test();
+
+        store
+            .update(CsbEvent::CreateOmission(sample_omission(
+                OmissionCategory::DeclarationsOfSupport(vec![ElectoralDistrict::UT]),
+            )))
+            .await?;
+
+        let status = RestorationStatus::for_declarations_of_support(&store);
+
+        assert!(status.has_omissions());
+        assert!(!status.has_corrections());
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn for_candidate_list_no_changes() -> Result<(), AppError> {
+        let store = CsbStore::new_for_test();
+
+        let list_id1 = CandidateListId::new();
+        let list_id2 = CandidateListId::new();
+
+        store.add_candidate_list(sample_candidate_list(list_id1));
+        store.add_candidate_list(sample_candidate_list(list_id2));
+
+        // add omission beloning to another list
+        store
+            .update(CsbEvent::CreateOmission(sample_omission(
+                OmissionCategory::CandidateList(vec![list_id2]),
+            )))
+            .await?;
+
+        let status = RestorationStatus::for_candidate_list(&store, list_id1).unwrap();
+
+        assert!(!status.has_omissions());
+        assert!(!status.has_corrections());
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn for_candidate_list_omission() -> Result<(), AppError> {
+        let store = CsbStore::new_for_test();
+
+        let list_id = CandidateListId::new();
+
+        store.add_candidate_list(sample_candidate_list(list_id));
+
+        store
+            .update(CsbEvent::CreateOmission(sample_omission(
+                OmissionCategory::CandidateList(vec![list_id]),
+            )))
+            .await?;
+
+        let status = RestorationStatus::for_candidate_list(&store, list_id).unwrap();
+
+        assert!(status.has_omissions());
+        assert!(!status.has_corrections());
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn for_candidate_list_containing_candidate_omission_and_correction()
+    -> Result<(), AppError> {
+        let store = CsbStore::new_for_test();
+
+        let list_id = CandidateListId::new();
+        let person_id = PersonId::new();
+
+        let mut list = sample_candidate_list(list_id);
+
+        list.candidates.push(person_id);
+
+        store.add_candidate_list(list);
+        store.add_person(sample_person(person_id));
+
+        store
+            .update(CsbEvent::CreateOmission(sample_omission(
+                OmissionCategory::Candidate {
+                    person: person_id,
+                    lists: vec![list_id],
+                },
+            )))
+            .await?;
+
+        store
+            .update(CsbEvent::UpdateCorrection(Correction::Person(
+                person_id,
+                PersonCorrection::Initials(Initials::from_str("A.B.").unwrap()),
+            )))
+            .await?;
+
+        let status = RestorationStatus::for_candidate_list(&store, list_id).unwrap();
+
+        assert!(status.has_omissions());
+        assert!(status.has_corrections());
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn for_candidate_no_changes() -> Result<(), AppError> {
+        let store = CsbStore::new_for_test();
+
+        let list_id1 = CandidateListId::new();
+        let list_id2 = CandidateListId::new();
+
+        let person_id = PersonId::new();
+
+        let mut list1 = sample_candidate_list(list_id1);
+        let mut list2 = sample_candidate_list(list_id2);
+
+        list1.candidates.push(person_id);
+        list2.candidates.push(person_id);
+
+        store.add_person(sample_person(person_id));
+        store.add_candidate_list(sample_candidate_list(list_id1));
+        store.add_candidate_list(sample_candidate_list(list_id2));
+
+        // add omission for only 1 of the lists
+        store
+            .update(CsbEvent::CreateOmission(sample_omission(
+                OmissionCategory::Candidate {
+                    person: person_id,
+                    lists: vec![list_id1],
+                },
+            )))
+            .await?;
+
+        // retrieve status for the other list
+        let status = RestorationStatus::for_candidate(&store, person_id, list_id2);
+
+        assert!(!status.has_omissions());
+        assert!(!status.has_corrections());
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn for_candidate_omission_and_correction() -> Result<(), AppError> {
+        let store = CsbStore::new_for_test();
+
+        let list_id = CandidateListId::new();
+
+        let person_id = PersonId::new();
+
+        let mut list1 = sample_candidate_list(list_id);
+
+        list1.candidates.push(person_id);
+
+        store.add_person(sample_person(person_id));
+        store.add_candidate_list(sample_candidate_list(list_id));
+
+        // add omission for only 1 of the lists
+        store
+            .update(CsbEvent::CreateOmission(sample_omission(
+                OmissionCategory::Candidate {
+                    person: person_id,
+                    lists: vec![list_id],
+                },
+            )))
+            .await?;
+
+        store
+            .update(CsbEvent::UpdateCorrection(Correction::Person(
+                person_id,
+                PersonCorrection::Initials(Initials::from_str("A.B.").unwrap()),
+            )))
+            .await?;
+
+        // retrieve status for the other list
+        let status = RestorationStatus::for_candidate(&store, person_id, list_id);
+
+        assert!(status.has_omissions());
+        assert!(status.has_corrections());
+
+        Ok(())
+    }
+}
